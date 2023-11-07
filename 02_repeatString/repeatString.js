2c0ab0face14107a5528bd7ba0a3d6771cdd9381
Gerd Geco <gerdgeco@gmail.com>
2023-11-07 08:25:59 +0100

    install jest , finish first 2 exercises in full

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string , num) {
 
+    var finalString = "";
+
+    if (num < 0){
+        return "ERROR"
+    }
+
+    for (var i = 0 ; i < num ; i++){
+        finalString += string;
+    }
+    
+    return finalString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
