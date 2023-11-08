986b3b6d48af4d1b5677554fc6fc1b84961b93ff
Gerd Geco <gerdgeco@gmail.com>
2023-11-08 02:44:30 +0100

    finish third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    var reverseString="";
+    var stringArray = string.split("");
+    var arrayReversed = stringArray.reverse();
+    console.log(arrayReversed);
 
+    for (var i = 0; i < arrayReversed.length ; i++){
+
+        reverseString += arrayReversed[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
