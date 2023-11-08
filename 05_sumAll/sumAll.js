ed5c8bebcddcb3cfb612640de2dd3628df4265a1
Gerd Geco <gerdgeco@gmail.com>
2023-11-08 04:45:58 +0100

    finish 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,48 @@
-const sumAll = function() {
+function isNumber(element){
+    if (typeof element === "number"){
+        return true;
+    }
+    return false;
+}
 
+function isPositive(number){
+    if (number > 0){
+        return true;
+    }
+    return false;
+}
+
+
+const sumAll = function(number1,number2) {
+
+    var firstNumber = 0;
+    var secondNumber = 0;
+    var sum = 0;
+
+    if ((isNumber(number1) && isNumber(number2)) === false){
+        return "ERROR";
+    }  
+
+    if ((isPositive(number1) && isPositive(number2)) === false){
+        return "ERROR";
+    }
+
+    if (number1 < number2){
+        firstNumber = number1;
+        secondNumber = number2;
+    }
+
+    else{
+        firstNumber = number2;
+        secondNumber = number1;
+    }
+
+
+    for (var i = firstNumber; i < secondNumber + 1; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
