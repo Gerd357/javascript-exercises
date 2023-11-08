770210b5938bee094b34661c09f07fc90f487bca
Gerd Geco <gerdgeco@gmail.com>
2023-11-08 04:02:19 +0100

    solved first test exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
 
+
+    for (var i = 0; i < array.length; i++){
+
+        if (array[i] === element){
+            array = array.slice(0,i).concat(array.slice(i + 1));
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
