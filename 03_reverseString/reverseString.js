e93be7fa6d277c629c1295c756e4aaf7a3f90988
Gerd Geco <gerdgeco@gmail.com>
2023-11-08 02:48:52 +0100

    remove console log statements

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,7 +3,6 @@ const reverseString = function(string) {
     var reverseString="";
     var stringArray = string.split("");
     var arrayReversed = stringArray.reverse();
-    console.log(arrayReversed);
 
     for (var i = 0; i < arrayReversed.length ; i++){
 
